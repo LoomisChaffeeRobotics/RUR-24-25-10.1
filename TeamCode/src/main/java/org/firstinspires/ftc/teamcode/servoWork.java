680b90dc2144b03aeda20d8cc4fa79f9589d7c53
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2024-11-15 15:48:34 -0500

    asdfghjkl;

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoWork.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoWork.java
@@ -2,45 +2,67 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class servoWork extends OpMode{
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor rightRear;
+    DcMotor leftRear;
     Servo servo1;
     Servo servo2;
+    Servo servo3;
+    Servo servo4;
     boolean armTogged = true;
-    boolean aPressed = false;
+
     @Override
     public void init() {
         servo2 = hardwareMap.get(Servo.class, "ArmR");
         servo1 = hardwareMap.get(Servo.class, "Claw");
+        servo3 = hardwareMap.get(Servo.class, "ArmL");
+        servo4 = hardwareMap.get(Servo.class, "Extender");
+        leftFront = hardwareMap.get(DcMotor.class, "frontLeft");
+        rightFront = hardwareMap.get(DcMotor.class, "frontRight");
+        rightRear = hardwareMap.get(DcMotor.class, "rearRight");
+        leftRear = hardwareMap.get(DcMotor.class, "rearLeft");
     }
 
     @Override
     public void loop() {
-        telemetry.addData("claw pos: ", servo1.getPosition());
-        telemetry.addData("ArmR pos: ", servo2.getPosition());
+        telemetry.addData("ArmL pos: ", servo1.getPosition());
+        telemetry.addData("Extender pos: ", servo2.getPosition());
         if (gamepad1.x){
-            servo1.setPosition(.7);
-            servo2.setPosition(.53);
+            rightFront.setPower(1);
+        } else {
+            rightFront.setPower(0);
 
         }
         if (gamepad1.b) {
-            servo1.setPosition(.3);
-            servo2.setPosition(.86);
+            leftFront.setPower(1);
+        } else {
+            leftFront.setPower(0);
         }
         if (gamepad1.y){
-            servo2.setPosition(servo2.getPosition()+0.001);
+            leftRear.setPower(1);
+        } else {
+            leftRear.setPower(0);
         }
         if (gamepad1.a){
-            if (aPressed) {
-                armToggle();
-                aPressed = true;
-            }
-
-        }else {
-            aPressed=false;
+            rightRear.setPower(1);
+        } else {
+            rightRear
+                    .setPower(0);
         }
+
+        telemetry.addData("RR", rightRear.getCurrentPosition());
+        telemetry.addData("LR", leftRear.getCurrentPosition());
+        telemetry.addData("RF", rightFront.getCurrentPosition());
+        telemetry.addData("LF", leftFront.getCurrentPosition());
+
+
+
     }
     public void armUp(){
         servo1.setPosition(.3);
