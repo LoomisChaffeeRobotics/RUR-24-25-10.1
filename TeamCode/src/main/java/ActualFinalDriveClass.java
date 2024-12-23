a506b525c597aac1abe84ba78e2e189508085d01
violin2024 <musichu20171819@gmail.com>
2024-12-23 10:59:34 -0500

    IT'S DPADUPDEPRESSED NOT DPDUPDEPRESSESED

--- TeamCode/src/main/java/ActualFinalDriveClass.java
+++ TeamCode/src/main/java/ActualFinalDriveClass.java
@@ -1,11 +1,7 @@
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
@@ -19,9 +15,7 @@ public class ActualFinalDriveClass extends OpMode {
     SampleMecanumDrive drive;
     boolean rbdepressed = false;
     double armLPos;
-    boolean dpdupdepressesed = false;
-    boolean ydepressed = false;
-    boolean adepressed = false;
+    boolean dpadupdepressed = false;
 
     @Override
     public void init() {
@@ -72,13 +66,13 @@ public class ActualFinalDriveClass extends OpMode {
             imu.resetYaw();
         }
         if (gamepad1.dpad_up){
-            if (!dpdupdepressesed){
+            if (!dpadupdepressed){
                 servos.armToggle();
-                dpdupdepressesed= true;
+                dpadupdepressed = true;
             }
 
         } else{
-            dpdupdepressesed= false;
+            dpadupdepressed = false;
         }
         if (gamepad1.dpad_down){
             servos.armSpecimen();
