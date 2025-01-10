cb99bf458855002a54f0d06980c98a253c42cf57
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2025-01-10 13:46:26 -0500

    Revert "IT'S DPADUPDEPRESSED NOT DPDUPDEPRESSESED"
    This reverts commit a506b525c597aac1abe84ba78e2e189508085d01.

--- TeamCode/src/main/java/ActualFinalDriveClass.java
+++ TeamCode/src/main/java/ActualFinalDriveClass.java
@@ -1,7 +1,11 @@
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
@@ -15,7 +19,9 @@ public class ActualFinalDriveClass extends OpMode {
     SampleMecanumDrive drive;
     boolean rbdepressed = false;
     double armLPos;
-    boolean dpadupdepressed = false;
+    boolean dpdupdepressesed = false;
+    boolean ydepressed = false;
+    boolean adepressed = false;
 
     @Override
     public void init() {
@@ -66,13 +72,13 @@ public class ActualFinalDriveClass extends OpMode {
             imu.resetYaw();
         }
         if (gamepad1.dpad_up){
-            if (!dpadupdepressed){
+            if (!dpdupdepressesed){
                 servos.armToggle();
-                dpadupdepressed = true;
+                dpdupdepressesed= true;
             }
 
         } else{
-            dpadupdepressed = false;
+            dpdupdepressesed= false;
         }
         if (gamepad1.dpad_down){
             servos.armSpecimen();
