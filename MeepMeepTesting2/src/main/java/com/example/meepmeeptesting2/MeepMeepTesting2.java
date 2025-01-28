cc4fe54698dffba7d2fef598db0993fd45c9546f
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2025-01-28 16:24:37 -0500

    Work

--- MeepMeepTesting2/src/main/java/com/example/meepmeeptesting2/MeepMeepTesting2.java
+++ MeepMeepTesting2/src/main/java/com/example/meepmeeptesting2/MeepMeepTesting2.java
@@ -81,3 +81,4 @@ public class MeepMeepTesting2 {
                 .start();
     }
 }
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/AutonModes/SampleSide.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.drive.AutonModes;
+
+import com.acmerobotics.roadrunner.drive.MecanumDrive;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.servoWork;
+
+@Autonomous
+public class SampleSide extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        servoWork  servos = new servoWork();
+
+        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
+                .strafeRight(10)
+                .forward(5)
+                .build();
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        drive.followTrajectory(myTrajectory);
+
+
+    }
+}
