affff96f1cddec965a5766596e06244207bca357
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2025-02-10 20:25:46 -0500

    arm stuff now enum - sounds cooler
    this is to sound fancy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoWork.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoWork.java
@@ -1,9 +1,6 @@
 package org.firstinspires.ftc.teamcode;
-import static com.qualcomm.robotcore.hardware.Servo.Direction.FORWARD;
 import static com.qualcomm.robotcore.hardware.Servo.Direction.REVERSE;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -11,11 +8,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import org.apache.commons.math3.stat.descriptive.rank.Max;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-import java.sql.Time;
-import java.util.Timer;
+
 public class servoWork {
     ElapsedTime elapsedTime;
     public Servo Claw;
@@ -33,7 +26,14 @@ public class servoWork {
     double LRP = 0; //linear slide power, LRP because Millen named that
     IMU imu;
     public double armDownPercent;
-    int armDowning = 0;
+    enum armState {
+        UP,
+        HALF,
+        SPECIMEN,
+        SAMPLE,
+        NULL
+    }
+    armState armDowning = armState.UP;
 
 
     //    SampleMecanumDrive drive;
@@ -63,49 +63,51 @@ public class servoWork {
         Tilter.setPower(1);
     }
     public void armUp(){
-        armDowning = 0;
+        armDowning = armState.UP;
         armTogged = false;
     }
     public void armDown(){
-        armDowning = 1;
+        armDowning = armState.SAMPLE;
         armTogged = true;
     }
     public void armUpdate(){
-        if (armDowning == 1){
+        if (armDowning == armState.SAMPLE){
             armDownPercent = Math.min(armDownPercent+0.02,1);
-        } else if (armDowning == 0){
+        } else if (armDowning == armState.UP){
             armDownPercent = Math.max(armDownPercent-0.02,0);
-        } else if (armDowning == 2) { // speciman
+        } else if (armDowning == armState.HALF) { // speciman
             if(armDownPercent <= .75){
                 armDownPercent = Math.min(0.75,armDownPercent+0.02);
             } else {
                 armDownPercent = Math.max(0.75,armDownPercent-0.02);
             }
-        } else if (armDowning == 3) {
+        } else if (armDowning == armState.SPECIMEN) {
             if(armDownPercent <= .94){
                 armDownPercent = Math.min(0.94,armDownPercent+0.02);
             } else {
                 armDownPercent = Math.max(0.94,armDownPercent-0.02);
             }
-        } else if (armDowning == 4) {
-            if(armDownPercent <= -.15){
-                armDownPercent = Math.min(-0.15,armDownPercent-0.01);
-            }
+
+//            TODO: delete this.
+//        } else if (armDowning == armState.?) {
+//            if(armDownPercent <= -.15){
+//                armDownPercent = Math.min(-0.15,armDownPercent-0.01);
+//            }
         }
         ArmL.setPosition(0.09*(1-armDownPercent)+(.3867*armDownPercent));
         ArmR.setPosition(0.6317*(1-armDownPercent)+(0.3233*armDownPercent));
 
     }
     public void armSpecimen(){
-        armDowning = 3;
+        armDowning = armState.SPECIMEN;
         armTogged = true;
 
     }
     public void arm45(){
-        armDowning = 2;
+        armDowning = armState.HALF;
     }
     public void armHang() {
-        armDowning = 4;
+        armDowning = armState.UP;
     }
     public void armToggle(){
 
