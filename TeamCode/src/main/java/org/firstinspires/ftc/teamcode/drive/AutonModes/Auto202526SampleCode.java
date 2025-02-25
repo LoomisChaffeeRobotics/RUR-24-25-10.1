bd7683ce95ae3a59024a6feb446ac7f25862d8ff
violin2024 <musichu20171819@gmail.com>
2025-02-25 14:42:43 -0500

    Created 2 classes as framework for next year
    Auto + DriveClass(including field centric and potentially working camera)
    
    Last commit of the season?  :(

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/AutonModes/Auto202526SampleCode.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.drive.AutonModes;
+
+import static java.lang.Math.PI;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.servoWork;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
+
+@Autonomous
+@Config
+
+public class Auto202526SampleCode extends LinearOpMode {
+    IMU imu;
+    TrajectorySequenceBuilder TrajectorySequenceBuilder;
+    ElapsedTime timer;
+    ElapsedTime timer2;
+    SampleMecanumDrive drive;
+    servoWork servos;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        servos = new servoWork();
+        servos.init(hardwareMap);
+        Pose2d startPose = new Pose2d(-24, -64.5, Math.toRadians(90));
+        TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(new Pose2d(-24,-64.5, Math.toRadians(90)))
+                .lineTo(new Vector2d(-50,-46))
+                .build();
+        drive.setPoseEstimate(startPose);
+        drive.followTrajectorySequenceAsync(trajSeq);
+
+
+        waitForStart();
+
+
+        if (isStopRequested()) return;
+        while (opModeIsActive()) {
+
+            drive.update();
+
+        }
+
+    }
+
+}
+
